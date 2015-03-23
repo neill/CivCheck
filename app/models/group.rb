6e978fda6c23c030e4d6c3bc4ec9b0f8b4dac88d
neillsilva <blackvector@gmail.com>
2015-03-22 20:23:50 -0400

    ID number randomized.

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,11 @@
 class Group < ActiveRecord::Base
     serialize :steamid_name
+    before_create :randomize_id
+
+    private
+    def randomize_id
+        begin
+            self.id = SecureRandom.random_number(10_000_000)
+        end while Group.where(id: self.id).exists?
+    end
 end
